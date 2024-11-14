627f1682432e8812ab5ff2758d3b09acfd654715
Kaleem315 <Kaleemullahmemon@gmail.com>
2024-11-15 04:49:09 +0500

    Completed part 3 of JS Exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let splitWord = word.split("");
+let reverseWord = splitWord.reverse();
+let requiredString = reverseWord.join("");
+return requiredString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
