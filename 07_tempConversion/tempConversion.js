df9c6124e6504ce632682e666056213cb37da25b
Kaleem315 <Kaleemullahmemon@gmail.com>
2024-12-07 04:47:14 +0500

    completed the exercise temp tempConversion by TOP.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let toCelsius = Math.round(((tempF - 32)*(5/9))*10)/10;
+  
+  console.log(`conversion to celsius is ${toCelsius} degrees`);
+  return toCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let toFarenheit = Math.round(((tempC*(9/5)) + 32)*10)/10;
+  console.log(`conversion to farenheit is ${toFarenheit} degrees`);
+  return toFarenheit;
 };
+
+console.log(convertToCelsius(98))
+console.log(convertToFahrenheit(0))
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
